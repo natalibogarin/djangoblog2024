9423375296a3395745be1cc8bff23026b38e177b
natalibogarin <natali.bogarin@gmail.com>
2024-10-09 23:08:34 -0300

    Actualizado templates + urls + views de Autenticación(blog_auth)

--- myblog/settings/base.py
+++ myblog/settings/base.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'blog'
+    'apps.blog',
+    'apps.blog_auth'
 ]
 
 MIDDLEWARE = [
